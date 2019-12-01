65fab16f609899937ab2cb5553c5cbef8f71abe3
Hui <jeffrey.x.hui@gmail.com>
2019-11-30 16:20:30 -0800

    comment planning complete, ready to begin building context API

--- src/App.js
+++ src/App.js
@@ -47,3 +47,7 @@ function App() {
 }
 
 export default App;
+// need to pass products, cart, and addItem to individual components being rendered from App.js
+// however I only know how to : 
+// <ShoppingContext.Provider state={cart}><ShoppingCart></ShoppingContext>
+// setting up Provider with one state, but not the rest
